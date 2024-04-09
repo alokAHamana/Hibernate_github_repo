8639b539027c4ca6d4ecd0fdac823e409d44d52b
Alok <alokahamana@gmail.com>
2024-04-09 10:52:10 +0530

    now commit to master

--- hibernateexample/src/main/java/com/aa/model/User_Detail.java
+++ hibernateexample/src/main/java/com/aa/model/User_Detail.java
@@ -7,7 +7,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name="user_table")
 public class User_Detail {       //Pull request.................
-
+                                 // now in Master
 	@Id
 	private int userId;
 
